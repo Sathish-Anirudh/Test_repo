6d933274ee4ef65b50d239cd8923f1a3484109d2
Sathish-Anirudh <anirudh@mindmapdigital.ai>
2026-07-01 14:35:40 +0530

    Refactor SQL routes to improve security and structure

--- SQL.py
+++ SQL.py
@@ -5,146 +5,130 @@ import sqlite3
 app = Flask(__name__)
 
 
-db = sqlite3.connect("users.db")
-
-
-# SQL Injection 1: string concatenation
-@app.route("/user1")
-def user1():
-
-    username = request.args.get("username")
-
-    query = (
-        "SELECT * FROM users WHERE username='"
-        + username
-        + "'"
-    )
-
-    result = db.execute(query)
-
-    return str(result.fetchall())
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
 
 
 
-# SQL Injection 2: format()
-@app.route("/user2")
-def user2():
+@app.route("/one")
+def one():
 
-    user_id = request.args.get("id")
-
-    query = (
-        "SELECT * FROM users WHERE id={}"
-        .format(user_id)
-    )
-
-    result = db.execute(query)
-
-    return str(result.fetchall())
-
-
-
-# SQL Injection 3: f-string
-@app.route("/user3")
-def user3():
-
-    email = request.args.get("email")
-
-    query = f"""
-    SELECT *
-    FROM users
-    WHERE email='{email}'
-    """
-
-    result = db.execute(query)
-
-    return str(result.fetchall())
-
-
-
-# SQL Injection 4: percent formatting
-@app.route("/user4")
-def user4():
-
-    name = request.args.get("name")
-
-    query = (
-        "SELECT * FROM users WHERE name='%s'"
-        % name
-    )
-
-    result = db.execute(query)
-
-    return str(result.fetchall())
-
-
-
-# SQL Injection 5: execute directly with input
-@app.route("/user5")
-def user5():
-
-    uid = request.args.get("uid")
-
-    return str(
-        db.execute(
-            "SELECT * FROM users WHERE id=" + uid
-        ).fetchall()
-    )
-
-
-
-# SQL Injection 6: raw cursor
-@app.route("/admin")
-def admin():
-
-    role = request.args.get("role")
-
-    cursor = db.cursor()
+    user = request.args.get("user")
 
     cursor.execute(
-        "SELECT * FROM users WHERE role='"
-        + role
-        + "'"
+        "SELECT * FROM users WHERE name='%s'" % user
     )
 
-    return str(cursor.fetchall())
+    return "ok"
 
 
 
-# SQL Injection 7: update query
-@app.route("/update")
-def update():
-
-    value = request.args.get("value")
-
-    query = (
-        "UPDATE users SET name='"
-        + value
-        + "'"
-    )
-
-    db.execute(query)
-
-    return "updated"
-
-
-
-# SQL Injection 8: delete query
-@app.route("/delete")
-def delete():
+@app.route("/two")
+def two():
 
     uid = request.args.get("id")
 
     query = (
-        "DELETE FROM users WHERE id="
+        "SELECT * FROM accounts WHERE id="
         + uid
     )
 
-    db.execute(query)
+    cursor.execute(query)
 
-    return "deleted"
+    return "ok"
 
 
 
-if __name__ == "__main__":
+@app.route("/three")
+def three():
 
-    app.run()
+    name = request.args.get("name")
+
+    query = f"""
+    SELECT *
+    FROM users
+    WHERE username = '{name}'
+    """
+
+    cursor.execute(query)
+
+    return "ok"
+
+
+
+@app.route("/four")
+def four():
+
+    email = request.args["email"]
+
+    query = (
+        "SELECT * FROM users "
+        "WHERE email='{}'"
+        .format(email)
+    )
+
+    cursor.execute(query)
+
+    return "ok"
+
+
+
+@app.route("/five")
+def five():
+
+    data = request.args.get("data")
+
+    sql = (
+        "INSERT INTO logs VALUES ('"
+        + data
+        + "')"
+    )
+
+    cursor.execute(sql)
+
+    return "ok"
+
+
+
+@app.route("/six")
+def six():
+
+    user_id = request.args.get("uid")
+
+    cursor.execute(
+        "DELETE FROM users WHERE id="
+        + user_id
+    )
+
+    return "ok"
+
+
+
+@app.route("/seven")
+def seven():
+
+    search = request.args.get("q")
+
+    cursor.execute(
+        "SELECT * FROM products WHERE name LIKE '%"
+        + search
+        + "%'"
+    )
+
+    return "ok"
+
+
+
+@app.route("/eight")
+def eight():
+
+    sort = request.args.get("sort")
+
+    query = (
+        "SELECT * FROM users ORDER BY "
+        + sort
+    )
+
+    cursor.execute(query)
+
+    return "ok"
